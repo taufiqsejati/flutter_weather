7284f1f70ee8f65f181f7ab060a668757cef2983
Muhammad Taufiq <taufiq@siklus.id>
2021-09-17 17:12:57 +0700

    first commit

---
+++ lib/weather/cubit/weather_cubit.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_cubit.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WeatherState _$WeatherStateFromJson(Map<String, dynamic> json) {
+  return $checkedNew('WeatherState', json, () {
+    final val = WeatherState(
+      status: $checkedConvert(
+          json, 'status', (v) => _$enumDecode(_$WeatherStatusEnumMap, v)),
+      temperatureUnits: $checkedConvert(json, 'temperature_units', (v) => v),
+      weather: $checkedConvert(json, 'weather', (v) => v),
+    );
+    return val;
+  }, fieldKeyMap: const {'temperatureUnits': 'temperature_units'});
+}
+
+Map<String, dynamic> _$WeatherStateToJson(WeatherState instance) =>
+    <String, dynamic>{
+      'status': _$WeatherStatusEnumMap[instance.status],
+      'weather': instance.weather,
+      'temperature_units': instance.temperatureUnits,
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+const _$WeatherStatusEnumMap = {
+  WeatherStatus.initial: 'initial',
+  WeatherStatus.loading: 'loading',
+  WeatherStatus.success: 'success',
+  WeatherStatus.failure: 'failure',
+};
